2944c26ad221c99a8191a1f8eff470f83e103208
Artem <awrer1234eads@gmail.com>
2022-07-28 00:29:50 +0300

    replace unchanging result component to routers switcher

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Results from "./components/Results";
+import RoutePaths from "./components/Router";
 
 function App() {
   const [darkTheme, setDarkTheme] = useState<boolean>(false);
@@ -11,7 +12,7 @@ function App() {
     <div className={ darkTheme ? 'dark' : '' }>
       <div className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen">
         <Header darkTheme={ darkTheme } setDarkTheme={ setDarkTheme } />
-        <Results />
+        <RoutePaths />
         <Footer />
       </div>
     </div>
