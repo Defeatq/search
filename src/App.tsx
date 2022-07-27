3217d0c0d0798c6e5a78a872e30f9c9c59b65a3f
Artem <awrer1234eads@gmail.com>
2022-07-27 22:10:52 +0300

    create app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Results from "./components/Results";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState<boolean>(false);
+
   return (
-    <div>
-      Search
+    <div className={ darkTheme ? 'dark' : '' }>
+      <div className="bg-gray-100 dark:bg-gray-900 text-black dark:text-white min-h-screen">
+        <Header darkTheme={ darkTheme } setDarkTheme={ setDarkTheme } />
+        <Results />
+        <Footer />
+      </div>
     </div>
   )
 }
