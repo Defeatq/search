284f7294c01514fdef36932f767b07da48f00540
Artem <awrer1234eads@gmail.com>
2022-07-28 00:31:10 +0300

    wrap main header by link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 type Props = {
   darkTheme: boolean,
   setDarkTheme: (darkTheme: boolean) => void,
@@ -7,9 +9,11 @@ function Header({ darkTheme, setDarkTheme }: Props) {
   return (
     <div className="flex flex-row flex-wrap justify-center items-center p-4 pb-0 mb-6 border-b border-gray-300 dark:border-gray-700">
       <div className="flex justify-between items-center w-screen">
-        <h1 className="p-1.5 text-xl font-bold bg-sky-500 dark:bg-sky-100 text-gray-100 dark:text-gray-900 rounded">
-          Gugl
-        </h1>
+        <Link to="/">
+          <h1 className="p-1.5 text-xl font-bold bg-sky-500 dark:bg-sky-100 text-gray-100 dark:text-gray-900 rounded cursor-pointer">
+            Gugl
+          </h1>
+        </Link>
         <button onClick={ () => setDarkTheme(!darkTheme) } className="p-1 bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 border border-gray-300 rounded-full">
           { darkTheme ? 'Light' : 'Dark' }
         </button>
