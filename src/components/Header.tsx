884f59739b6565d474ac28d16919ed34ea5303ed
Artem <awrer1234eads@gmail.com>
2022-07-27 23:32:05 +0300

    add classes and tags to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ type Props = {
 
 function Header({ darkTheme, setDarkTheme }: Props) {
   return (
-    <div className="flex flex-row flex-wrap justify-center items-center p-4 pb-0 border-b border-gray-300 dark:border-gray-700">
+    <div className="flex flex-row flex-wrap justify-center items-center p-4 pb-0 mb-6 border-b border-gray-300 dark:border-gray-700">
       <div className="flex justify-between items-center w-screen">
-        <p className="p-1.5 text-xl font-bold bg-sky-500 dark:bg-sky-100 text-gray-100 dark:text-gray-900 rounded">
+        <h1 className="p-1.5 text-xl font-bold bg-sky-500 dark:bg-sky-100 text-gray-100 dark:text-gray-900 rounded">
           Gugl
-        </p>
+        </h1>
         <button onClick={ () => setDarkTheme(!darkTheme) } className="p-1 bg-gray-900 dark:bg-gray-100 text-gray-100 dark:text-gray-900 border border-gray-300 rounded-full">
           { darkTheme ? 'Light' : 'Dark' }
         </button>
