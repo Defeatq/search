3624cd38677acc21422a03d483276dd385cf4f13
Artem <awrer1234eads@gmail.com>
2022-07-27 23:31:25 +0300

    create footer layout and style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="flex justify-center items-center p-8 mt-10 border-t border-gray-300 dark:border-gray-700">
+      <h3>
+        2022 Gugl, Inc.
+      </h3>
     </div>
   )
 }
