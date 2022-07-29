81c915f074529082dd455fc350d294749f10bfa4
Artem <awrer1234eads@gmail.com>
2022-07-30 00:47:02 +0300

    create components for pages

---
+++ src/components/pages/All.tsx
@@ -0,0 +1,34 @@
+function All() {
+  return (
+    <div className="sm:px-24 md:px-36 lg:px-56">
+      <div className="flex flex-wrap justify-between items-center gap-y-6">
+        <a href="#" className="md:w-2/5 w-full">
+          <p className="break-words text-gray-200">
+            https://en.wikipedia.org/wiki/
+          </p>
+          <p className="break-words text-xl text-blue-300">
+            Elon Musk - Wikipedia
+          </p>
+        </a>
+        <a href="#" className="md:w-2/5 w-full">
+          <p className="break-words text-gray-200">
+            https://en.wikipedia.org/wiki/
+          </p>
+          <p className="break-words text-xl text-blue-300">
+            Elon Musk - Wikipedia
+          </p>
+        </a>
+        <a href="#" className="md:w-2/5 w-full">
+          <p className="break-words text-gray-200">
+            https://en.wikipedia.org/wiki/
+          </p>
+          <p className="break-words text-xl text-blue-300">
+            Elon Musk - Wikipedia
+          </p>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default All;
