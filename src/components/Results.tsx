370bc7bc8abe27a40683d9a84f035674628bfd9e
Artem <awrer1234eads@gmail.com>
2022-07-30 00:45:48 +0300

    make results depends on path location

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,9 +1,38 @@
+import { useLocation } from "react-router-dom";
+
+import All from "./pages/All";
+import Images from "./pages/Images";
+import News from "./pages/News";
+import Videos from "./pages/Videos";
+
+
 function Results() {
-  return (
-    <div>
-      Results
-    </div>
-  )
+  const locations = useLocation();
+
+  switch (locations.pathname) {
+    case '/search':
+      return (
+        <All />
+      )
+    case '/news':
+      return (
+        <News />
+      )
+    case '/images':
+      return (
+        <Images />
+      )
+    case '/videos':
+      return (
+        <Videos />
+      )
+    default:
+      return (
+        <div>
+          Unknown
+        </div>  
+      )
+  }
 }
 
 export default Results;
