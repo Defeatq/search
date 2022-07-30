5dc5ee9e3b28e756a307fbd8c20912f1f882fa38
Artem <awrer1234eads@gmail.com>
2022-07-30 03:15:34 +0300

    create news page layout

--- src/components/pages/News.tsx
+++ src/components/pages/News.tsx
@@ -1,7 +1,32 @@
 function News() {
   return (
-    <div>
-      News
+    <div className="sm:px-24 md:px-36 lg:px-56">
+      <div className="flex flex-wrap justify-between items-start gap-y-6">
+        <a href="#" className="flex flex-col md:w-2/5 w-full">
+          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quis, sed eius facilis eum rerum distinctio eligendi tenetur voluptates ratione. Distinctio, voluptates laborum adipisci voluptate blanditiis beatae at ducimus debitis?
+          </p>
+          <p className="break-words dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+        <a href="#" className="flex flex-col md:w-2/5 w-full">
+          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
+            Elon Musk - Wikipedia
+          </p>
+          <p className="break-words dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+        <a href="#" className="flex flex-col md:w-2/5 w-full">
+          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
+            Elon Musk - Wikipedia
+          </p>
+          <p className="break-words dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+      </div>
     </div>
   )
 }
