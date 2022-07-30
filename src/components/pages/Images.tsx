a48c2be7846dc4c88d518c6e19cdde5d51d36a35
Artem <awrer1234eads@gmail.com>
2022-07-30 03:17:05 +0300

    add margin for img wrapper

--- src/components/pages/Images.tsx
+++ src/components/pages/Images.tsx
@@ -1,6 +1,6 @@
 function Images() {
   return (
-    <div className="mx-auto">
+    <div className="mx-5">
       <div className="flex flex-wrap justify-start items-center gap-5">
         <a href="#" className="sm:max-w-[20%] lg:max-w-[15%] max-w-[40%] hover:underline">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU"/>
