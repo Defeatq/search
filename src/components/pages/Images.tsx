4858688eb135aedbb913302701bd802c22083a58
Artem <awrer1234eads@gmail.com>
2022-07-30 03:15:11 +0300

    create img page layout

--- src/components/pages/Images.tsx
+++ src/components/pages/Images.tsx
@@ -1,7 +1,35 @@
 function Images() {
   return (
-    <div>
-      Images
+    <div className="mx-auto">
+      <div className="flex flex-wrap justify-start items-center gap-5">
+        <a href="#" className="sm:max-w-[20%] lg:max-w-[15%] max-w-[40%] hover:underline">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU"/>
+          <p className="break-words text-sm dark:text-gray-200 text-gray-900">
+            Elon Musk - Wikipedia
+          </p>
+          <p className="break-words text-sm dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+        <a href="#" className="sm:max-w-[20%] lg:max-w-[15%] max-w-[40%] hover:underline">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU"/>
+          <p className="break-words dark:text-gray-200 text-gray-900">
+            Elon Musk - Wikipedia
+          </p>
+          <p className="break-words text-sm dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+        <a href="#" className="sm:max-w-[20%] lg:max-w-[15%] max-w-[40%] hover:underline">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU"/>
+          <p className="break-words dark:text-gray-200 text-gray-900">
+            Elon Musk - Wikipedia
+          </p>
+          <p className="break-words text-sm dark:text-gray-200 text-gray-900">
+            https://en.wikipedia.org/wiki/
+          </p>
+        </a>
+      </div>
     </div>
   )
 }
