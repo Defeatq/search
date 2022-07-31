49aa329cae57281837092055730ff65e22b888d9
Artem <awrer1234eads@gmail.com>
2022-08-01 01:40:33 +0300

    create news page

--- src/components/pages/News.tsx
+++ src/components/pages/News.tsx
@@ -1,31 +1,35 @@
-function News() {
+interface Result {
+  title: string,
+  link: string,
+}
+
+type Results = {
+  entries?: Result[];
+}
+
+type Props = {
+  results: Results | null | undefined,
+}
+
+function News(props: Props) {
+  const results = props.results;
+  const resultsArray = results?.entries;
+
   return (
     <div className="sm:px-24 md:px-36 lg:px-56">
       <div className="flex flex-wrap justify-between items-start gap-y-6">
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quis, sed eius facilis eum rerum distinctio eligendi tenetur voluptates ratione. Distinctio, voluptates laborum adipisci voluptate blanditiis beatae at ducimus debitis?
-          </p>
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-        </a>
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Elon Musk - Wikipedia
-          </p>
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-        </a>
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Elon Musk - Wikipedia
-          </p>
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-        </a>
+        {
+          resultsArray?.map((result, index) => 
+          <a key={ index } href={ result.link } target="_blank" className="flex flex-col md:w-2/5 w-full">
+            <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
+              { result.title }
+            </p>
+            <p className="break-words dark:text-gray-200 text-gray-900">
+              { result.link }
+            </p>
+          </a>
+          )
+        }
       </div>
     </div>
   )
