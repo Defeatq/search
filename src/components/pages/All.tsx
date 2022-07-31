c1225fb9fbaf03be80a31fd8a9c809c0f63b8322
Artem <awrer1234eads@gmail.com>
2022-08-01 01:30:17 +0300

    create all page

--- src/components/pages/All.tsx
+++ src/components/pages/All.tsx
@@ -1,31 +1,36 @@
-function All() {
+type Results = {
+  results?: Result[];
+};
+
+type Props = {
+  results: Results | null | undefined,
+};
+
+interface Result {
+  title: string,
+  link: string,
+  description: string,
+};
+
+function All(props: Props) {
+  const results = props.results;
+  const resultsArray = results?.results;
+
   return (
     <div className="sm:px-24 md:px-36 lg:px-56">
       <div className="flex flex-wrap justify-between items-start gap-y-6">
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Elon Musk - Wikipedia
-          </p>
-        </a>
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Elon Musk - Wikipedia
-          </p>
-        </a>
-        <a href="#" className="flex flex-col md:w-2/5 w-full">
-          <p className="break-words dark:text-gray-200 text-gray-900">
-            https://en.wikipedia.org/wiki/
-          </p>
-          <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
-            Elon Musk - Wikipedia
-          </p>
-        </a>
+        {
+          resultsArray?.map((result, index) => 
+            <a key={ index } href={ result.link } className="flex flex-col md:w-2/5 w-full">
+              <p className="break-words dark:text-gray-200 text-gray-900">
+                { result.link }
+              </p>
+              <p className="break-words text-xl dark:text-blue-300 text-blue-600 hover:underline">
+                { result.title }
+              </p>
+            </a>
+          )
+        }
       </div>
     </div>
   )
