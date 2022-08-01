9d50f0267fe027ceff39d0b77aeabba099348d9f
Artem <awrer1234eads@gmail.com>
2022-08-02 02:02:25 +0300

    create video page

--- src/components/pages/Videos.tsx
+++ src/components/pages/Videos.tsx
@@ -1,7 +1,32 @@
-function Videos() {
+import ReactPlayer from "react-player";
+
+interface Result {
+  link: string,
+}
+
+type Results = {
+  results?: Result[];
+}
+
+type Props = {
+  results: Results | null | undefined,
+}
+
+function Videos(props: Props) {
+  const results = props.results;
+  const videos = results?.results;
+
   return (
-    <div>
-      Videos
+    <div className="flex flex-wrap">
+      {
+        videos?.map((video, index) => 
+          video.link.includes('www.youtube.com') ?
+          (<div key={ index } className="p-3">
+            <ReactPlayer url={ video.link } controls width="355px" height="200px"/>
+          </div>) : 
+          <></>
+        )
+      }
     </div>
   )
 }
