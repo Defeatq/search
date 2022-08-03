dec76bb49b2e8addfce47c1054baffe8ed3b9115
Artem <awrer1234eads@gmail.com>
2022-08-03 16:13:51 +0300

    create value debouncer

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,24 +1,29 @@
-import { ChangeEvent, useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
+
 import { useResultContext } from "../assets/contexts/RequestResultsContextProvider";
 
 function Search() {
+  const [text, setText] = useState<string>('');
+  const [debouncedText] = useDebounce(text, 500);
   const context = useResultContext();
 
-  const searchingValue = context?.searchingValue;
+  // const searchingValue = context?.searchingValue;
   const setSearchingValue = context?.setSearchingValue;
 
   useEffect(() => {
-    console.log(searchingValue)
-  }, [searchingValue])
+    if (setSearchingValue) {
+      setSearchingValue(debouncedText);
+    }
+  }, [debouncedText]);
 
   return (
-    <form className="-mt-10">
+    <form className="-mt-10 w-96">
       <input 
-        value={ searchingValue }
-        onChange={ setSearchingValue ? 
-                  (event: ChangeEvent<HTMLInputElement>) => setSearchingValue(event.target.value) : 
-                  undefined } 
-        className="w-96 h-12 px-6 dark:text-gray-800 bg-gray-100 rounded-full dark:border-0 border border-gray-300"
+        type="search"
+        value={ text }
+        onChange={ (event: ChangeEvent<HTMLInputElement>) => setText(event.target.value) } 
+        className="w-full h-12 pl-6 pr-3 dark:text-gray-800 bg-gray-100 rounded-full dark:border-0 border border-gray-300"
       />
     </form>
   )
