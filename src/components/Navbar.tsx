809a42b64f60960d60d102f0096856d72e3cff6e
Artem <awrer1234eads@gmail.com>
2022-08-03 11:59:16 +0300

    create navbar components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Links from "./Links";
+import Search from "./Search";
+
+function Navbar() {
+  return (
+    <div className="">
+      <Search />
+      <Links />
+    </div>
+  )
+}
+
+export default Navbar;
